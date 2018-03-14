ebd8d2d7ca6be1b809e0f1d4c582e8649a740c32
Whit <whit.danger@gmail.com>
2018-03-14 14:16:22 -0600

    fixed bugs

--- config/connection.js
+++ config/connection.js
@@ -10,10 +10,10 @@ if (process.env.JAWSDB_URL) {
 } else {
 	// DB is local on localhost
 	connection = mysql.createConnection({
-		port: 3306,
+		port: 8889,
 		host: 'localhost',
 		user: 'root',
-		password: '',
+		password: 'root',
 		database: 'burgers_db'
 	})
 };
